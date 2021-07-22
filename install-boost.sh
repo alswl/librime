97034b1dc84ba7d8b9fa14b5e2b81ad13542af13
alswl <alswlx@gmail.com>
2021-07-23 01:27:42 +0800

    feat: using 1.68 boost

--- install-boost.sh
+++ install-boost.sh
@@ -3,15 +3,20 @@ set -ev
 
 RIME_ROOT="$(cd "$(dirname "$0")"; pwd)"
 
-boost_version="${boost_version=1.75.0}"
+boost_version="${boost_version=1.68.0}"
+# boost_version="${boost_version=1.75.0}"
 boost_x_y_z="${boost_version//./_}"
 
 BOOST_ROOT="${BOOST_ROOT=${RIME_ROOT}/thirdparty/src/boost_${boost_x_y_z}}"
 
 boost_tarball="boost_${boost_x_y_z}.tar.bz2"
-download_url="https://dl.bintray.com/boostorg/release/${boost_version}/source/${boost_tarball}"
-boost_tarball_sha256sum_1_75_0='953db31e016db7bb207f11432bef7df100516eeb746843fa0486a222e3fd49cb  boost_1_75_0.tar.bz2'
-boost_tarball_sha256sum="${boost_tarball_sha256sum=${boost_tarball_sha256sum_1_75_0}}"
+# issue [boost\_1\_70\_0.tar.gz is Forbidden · Issue #299 · boostorg/boost](https://github.com/boostorg/boost/issues/299)
+# download_url="https://dl.bintray.com/boostorg/release/${boost_version}/source/${boost_tarball}"
+download_url="https://nchc.dl.sourceforge.net/project/boost/boost/${boost_version}/${boost_tarball}"
+#boost_tarball_sha256sum_1_75_0='953db31e016db7bb207f11432bef7df100516eeb746843fa0486a222e3fd49cb  boost_1_75_0.tar.bz2'
+boost_tarball_sha256sum_1_68_0='7f6130bc3cf65f56a618888ce9d5ea704fa10b462be126ad053e80e553d6d8b7  boost_1_68_0.tar.bz2'
+# boost_tarball_sha256sum="${boost_tarball_sha256sum=${boost_tarball_sha256sum_1_75_0}}"
+boost_tarball_sha256sum="${boost_tarball_sha256sum=${boost_tarball_sha256sum_1_68_0}}"
 
 download_boost_source() {
     cd "${RIME_ROOT}/thirdparty/src"
